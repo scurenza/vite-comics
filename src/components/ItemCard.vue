519ae01c21ec798ff3c2e7115903fb3ec5083ce0
scurenza <fedechiar@hotmail.it>
2022-11-09 16:14:22 +0100

    problemi ItemCard

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: "ItemCard",
+    props: {
+        title: String,
+        src: String,
+        price: String
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="src" :alt="title">
+        <div class="card-text">
+            <div class="title">
+                <h4>{{ title }}</h4>
+            </div>
+            <div class="price">
+                <p>{{ price }}</p>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card{
+    width: calc(100% / 6 - 10px);
+}
+</style>
